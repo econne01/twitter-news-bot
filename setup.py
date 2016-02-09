7ce9d3eba1feadf604ccfe8ff0d1cadf2522bdf1
Eric Connelly <eric.connelly08@gmail.com>
2016-02-08 20:03:00 -0500

    Print a static Tweet to console

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'twitter-news-bot',
+    version = '0.1',
+    author = 'Eric Connelly',
+    description = 'A Twitter Bot to tweet about curated news content',
+    url = 'https://github.com/econne01/twitter-news-bot',
+    install_requires = [],
+    packages = find_packages(),
+)
---
+++ src/run.py
@@ -0,0 +1,12 @@
+"""This is the main function of twitter-news-bot project
+
+It is intended to be run as a cronjob to periodically scan
+for news of interest and Tweet about it
+"""
+
+def main():
+    print 'An interesting news headline'
+
+
+if __name__ == '__main__':
+    main()
