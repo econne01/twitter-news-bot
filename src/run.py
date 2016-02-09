f2cfefb4fa0910227512f1a879b6692485917ac1
Eric Connelly <eric.connelly08@gmail.com>
2016-02-08 20:59:16 -0500

    Add twitter API service file

--- src/run.py
+++ src/run.py
@@ -4,11 +4,14 @@ It is intended to be run as a cronjob to periodically scan
 for news of interest and Tweet about it
 """
 from twitter_bot.service.news_reader import NewsReader
+from twitter_bot.service.twitter import TwitterService
+
 
 def main():
     news_reader = NewsReader()
     headlines = news_reader.get_headlines()
-    print headlines[0]
+    twitter_api = TwitterService()
+    twitter_api.post_tweet(headlines[0][:140])
 
 
 if __name__ == '__main__':
---
+++ src/twitter_bot/service/twitter.py
@@ -0,0 +1,8 @@
+class TwitterService(object):
+
+    def post_tweet(self, tweet):
+        """Post a tweet to Twitter
+
+        @param {String} tweet
+        """
+        print tweet
