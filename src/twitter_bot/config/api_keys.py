5e11650da7b930e0193ffcf778f80af4d8da1f87
Eric Connelly <econnelly@amplify.com>
2016-02-10 18:37:40 -0500

    Add Google API key

--- src/twitter_bot/config/api_keys.py
+++ src/twitter_bot/config/api_keys.py
@@ -1,10 +1,14 @@
+GOOGLE_API_KEYS = {
+    'URL_SHORTENER_API_KEY': 'url-shortener-api-key-value'
+}
+
 NEW_YORK_TIMES_API_KEYS = {
     'TOP_STORIES' : 'top-stories-value'
 }
 
 TWITTER_API_KEYS = {
-    'API_KEY' : 'api-key-value'
-    'API_SECRET' : 'api-secret-value'
-    'ACCESS_TOKEN' : 'access-token-value'
+    'API_KEY' : 'api-key-value',
+    'API_SECRET' : 'api-secret-value',
+    'ACCESS_TOKEN' : 'access-token-value',
     'ACCESS_SECRET' : 'access-secret-value'
 }
