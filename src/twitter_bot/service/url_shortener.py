ab9184243df13fb1ab130a334a3c66283cc79fda
Eric Connelly <econnelly@amplify.com>
2016-02-10 18:33:18 -0500

    Add support for Google URL Shortener API

---
+++ src/twitter_bot/service/url_shortener.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+from twitter_bot.config.api_keys import GOOGLE_API_KEYS
+
+
+def get_short_url(long_url):
+    """Call Google URL Shortener API to return shortened URL"""
+    api_key = GOOGLE_API_KEYS['URL_SHORTENER_API_KEY']
+    base_url = 'https://www.googleapis.com/urlshortener/v1/url'
+    headers = {'Content-Type':'application/json'}
+    post_data = json.dumps({"longUrl": long_url})
+    response = requests.post(base_url, params={'key': api_key}, data=post_data, headers=headers)
+    return response.json()['id']
