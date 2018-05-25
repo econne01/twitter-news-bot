324cf60542587dbf3ae62cb497d5d34a2c936c3d
Eric Connelly <eric.connelly08@gmail.com>
2018-05-25 19:26:18 -0400

    Trying to update setup.py to support requirements.txt file but doesnt seem to work

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+
+def get_requirements(file_path):
+    """Reads the installation requirements from requirements.txt"""
+    with open(file_path) as reqfile:
+        reqs = filter(lambda line: not line.startswith(('#', '-')), reqfile.read().split("\n"))
+        return list(reqs)
+
 setup(
     name = 'twitter-news-bot',
     version = '0.1',
     author = 'Eric Connelly',
     description = 'A Twitter Bot to tweet about curated news content',
     url = 'https://github.com/econne01/twitter-news-bot',
-    install_requires = [
-        'oauth2>=1.9,<2',
-        'requests>=2.9,<3.0'
-    ],
+    install_requires=get_requirements("requirements.txt"),
     packages = find_packages(),
 )
